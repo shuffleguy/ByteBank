51ce0d5cf2f21685b7aa97c034a98ad4361ea161
lvinicio <lucas.vinicius.oliveira@everis.com>
2021-03-16 16:31:45 -0300

    criação do estrututra de repetição

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,14 +6,27 @@ fun main(){
     val numeroConta: Int = 1000
     var saldo: Double = 0.0
 
-    saldo = 100.toDouble()//conversão para double
-
-    saldo +=200
+//    saldo = 100.toDouble()//conversão para double
+//
+//    saldo +=200
+    saldo = -1.0
 
     titular = "Alex 'lelek' Felipe"
 
     println("Titular $titular")//String Template
     println("numero da conta $numeroConta")
     println("saldo da conta $saldo")
+
+    if (saldo > 0.0){
+        println("Conta está positiva")
+
+    }else if (saldo == 0.0){
+
+        println("Conta está zerada")
+
+    }else{
+
+        println("Está negativada")
+    }
 
 }
