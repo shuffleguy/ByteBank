cf8edeba4d83b1a7f00e61e4a77e30e05a4b3dd0
lvinicio <lucas.vinicius.oliveira@everis.com>
2021-03-16 16:21:01 -0300

    Criação de var a função main

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,8 +2,18 @@ fun main(){
 
     println("Bem vindo ao Bytebank")
 
-    val titular = "Alex"
+    val titular: String
+    val numeroConta: Int = 1000
+    var saldo: Double = 0.0
 
-    print("Titular $titular")//String Template
+    saldo = 100.toDouble()//conversão para double
+
+    saldo +=200
+
+    titular = "Alex 'lelek' Felipe"
+
+    println("Titular $titular")//String Template
+    println("numero da conta $numeroConta")
+    println("saldo da conta $saldo")
 
 }
