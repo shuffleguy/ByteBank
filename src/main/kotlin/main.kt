859d5a2c5100446fe977c9da5a3526bb35d479ec
lvinicio <lucas.vinicius.oliveira@everis.com>
2021-03-16 18:00:56 -0300

    transformando de procedural para Orientada a objetos

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,35 @@
 fun main() {
 
     println("Bem vindo ao Bytebank")
+
+    geraConta(2)
+
+
+    val contaAlex = Conta()
+    contaAlex.titular = "lelek"
+    contaAlex.numero = 1000
+    println(contaAlex.titular)
+
+    val contaFran = Conta()
+    contaFran.titular = "Fran"
+    println(contaFran.titular)
+}
+
+class Conta(){
+
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+
+}
+fun geraConta(numero:Int) {
+    for (i in 1..numero) {
+        println(1235+i)
+
+    }
+    println()
+}
+fun testaLacos(){
     var i = 0
 
     while ( i < 5){
@@ -13,38 +42,7 @@ fun main() {
         println()
         i++
     }
-    }
-
-
-
-
-//    for (i in 1 .. 10) {//for (i in 5 downTo 1 step 2) {para criar decrescente
-//
-//        if (i==5){
-//            break
-//        }
-//
-//        val titular: String = "$i"
-//        val numeroConta: Int = 0 + i
-//        var saldo: Double = 0.0
-//
-////        saldo = 100.toDouble()//conversão para double
-////        saldo += 200
-////        saldo = -1.0
-//
-//
-//
-//        println("Titular $titular")//String Template
-//        println("numero da conta $numeroConta")
-//        println("saldo da conta $saldo")
-//        println()
-//
-//}
-//
-//
-////        testaCondicoes(saldo)
-//
-//    }
+}
 
     fun testaCondicoes(saldo: Double) {//aplicação do paramentro
 // utilizar variável pois if e when chamam
