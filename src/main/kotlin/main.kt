e6b8f6790e303fb5ff273e7ce6342e0a3eeed8e0
lvinicio <lucas.vinicius.oliveira@everis.com>
2021-03-17 08:41:21 -0300

    :zap: adicionando primeiros getters and setters

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,6 @@
 fun main() {
 
     println("Bem vindo ao Bytebank")
-
-
 //    var lista = arrayOf(geraConta(2))
 //    println(lista)
 
@@ -10,60 +8,58 @@ fun main() {
     val contaAlex = Conta()
     contaAlex.titular = "lelek"
     contaAlex.numero = 1000
-    contaAlex.saldo = 500.0
+    contaAlex.setSaldo(500.0)
     println(contaAlex.titular)
     println(contaAlex.numero)
-    println(contaAlex.saldo)
+    println(contaAlex.getSaldo())
 
-    contaAlex.deposita(150.0)//chamando como objeto
-    println("Saldo atual: ${contaAlex.saldo}")
+//    contaAlex.deposita(150.0)//chamando como objeto
+//    println("Saldo atual: ${contaAlex.saldo}")
 
 
     val contaFran = Conta()
     contaFran.titular = "Fran"
     contaFran.numero = 1001
-    contaFran.saldo = 230.0
+    contaFran.setSaldo(230.0)
     println(contaFran.titular)
     println(contaFran.numero)
-    println(contaFran.saldo)
+    println(contaFran.getSaldo())
 
-    contaFran.deposita( 15.0)
-
-
-
-    println("Saldo atual: ${contaFran.saldo}")
-
-    contaFran.saque(405.0)
-
-    if (contaAlex.transfere(100.0,contaFran)){
-        println("Transferencia sucessida")
-        }else
-            println("saldo insuficiente")
+//    contaFran.deposita( 15.0)
+//    println("Saldo atual: ${contaFran.saldo}")
+//
+//    contaFran.saque(405.0)
+//
+//    if (contaAlex.transfere(100.0,contaFran)){
+//        println("Transferencia sucessida")
+//        }else
+//            println("saldo insuficiente")
 }
 
-class Conta(){
+class Conta() {
 
     var titular = ""
     var numero = 0
-    var saldo = 0.0
+    private var saldo = 0.0
 
-    fun deposita (valor: Double){// transformando em método
+    fun deposita(valor: Double) {// transformando em método
         this.saldo += valor
     }
-    fun saque (valor: Double){
-        if (this.saldo>=valor){
+
+    fun saque(valor: Double) {
+        if (this.saldo >= valor) {
             println("Operação de saque selecionada...")
-            this.saldo-=valor
+            this.saldo -= valor
             println("Saldo atual $saldo")
-            if (saldo == 0.0){
+            if (saldo == 0.0) {
                 println("Seu saldo está zerado")
             }
         }
-            return println("Saldo insuficiente")//else desnecessário
+        return println("Saldo insuficiente")//else desnecessário
     }
 
-    fun transfere(valor: Double, destino: Conta): Boolean{
-        if (this.saldo >= valor){
+    fun transfere(valor: Double, destino: Conta): Boolean {
+        if (this.saldo >= valor) {
             this.saldo -= valor
             println("saldo atual: ${this.saldo}")
             println("transferindo: $valor")
@@ -71,23 +67,35 @@ class Conta(){
             return true
         }
         return false
+    }
 
+    fun getSaldo(): Double {
+        return saldo
+    }
+    fun setSaldo(valor: Double){
+       if(valor >= 0.0){
+           this.saldo = valor
+       } else{
+           println("Não se pode setar negativo")
+           
+       }
 
 
     }
 }
 
-fun geraConta(numero:Int) {
+fun geraConta(numero: Int) {
     for (i in 1..numero) {
-        println(1235+i)
+        println(1235 + i)
 
     }
     println()
 }
-fun testaLacos(){
+
+fun testaLacos() {
     var i = 0
 
-    while ( i < 5){
+    while (i < 5) {
         val titular: String = "$i"
         val numeroConta: Int = 0 + i
         var saldo: Double = 0.0
@@ -99,24 +107,24 @@ fun testaLacos(){
     }
 }
 
-    fun testaCondicoes(saldo: Double) {//aplicação do paramentro
+fun testaCondicoes(saldo: Double) {//aplicação do paramentro
 // utilizar variável pois if e when chamam
 
-        if (saldo > 0.0) {
-            println("Conta está positiva")
+    if (saldo > 0.0) {
+        println("Conta está positiva")
 
-        } else if (saldo == 0.0) {
+    } else if (saldo == 0.0) {
 
-            println("Conta está zerada")
+        println("Conta está zerada")
 
-        } else {
+    } else {
 
-            println("Está negativada")
-        }
-        when {
-            saldo > 0.0 -> println("Conta está positiva")
-            saldo == 0.0 -> println("Conta está zerada")
-            else -> println("Está negativada")
-        }
-
+        println("Está negativada")
     }
+    when {
+        saldo > 0.0 -> println("Conta está positiva")
+        saldo == 0.0 -> println("Conta está zerada")
+        else -> println("Está negativada")
+    }
+
+}
